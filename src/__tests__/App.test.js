30be01ec7014b4ec8285a1d944089665666675eb
Hal Fulcher <hallam.fulcher@gmail.com>
2021-03-14 21:49:44 +0000

    More testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,21 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import App from "../components/App";
 
-describe("App", () => {
-  const { asFragment } = render(<App />);
-
-  it("renders correctly", () => {
+describe("<App />", () => {
+  it("renders and matches snapshot", () => {
+    const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders as expected", () => {
+    const { getByTestId } = render(<App />);
+    const app = getByTestId("app");
+    expect(app).toBeTruthy();
+  });
+
+  it("renders logo", () => {
+    const { getByTestId } = render(<App />);
+    const logo = getByTestId("logo");
+    expect(logo).toBeTruthy();
   });
 });
--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -1,11 +1,31 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import Search from "../components/Search";
 
-describe("Search", () => {
-  const { asFragment } = render(<Search />);
+describe("<Search />", () => {
+  const validProps = {
+    setSearchResults: () => {},
+  };
 
-  it("renders correctly", () => {
+  it("renders and matches snapshot", () => {
+    const { asFragment } = render(
+      <Search setSearchResults={validProps.setSearchResults} />
+    );
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders search input as expected", () => {
+    const { getByTestId } = render(
+      <Search setSearchResults={validProps.setSearchResults} />
+    );
+    const input = getByTestId("search-input");
+    expect(input).toBeTruthy();
+  });
+
+  it("renders search button as expected", () => {
+    const { getByTestId } = render(
+      <Search setSearchResults={validProps.setSearchResults} />
+    );
+    const button = getByTestId("search-btn");
+    expect(button).toBeTruthy();
   });
 });
