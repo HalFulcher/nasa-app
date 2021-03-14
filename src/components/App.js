ea0d856a1f65759f9ca10040fd75e16b3036e9f3
Hal Fulcher <hallam.fulcher@gmail.com>
2021-03-14 21:59:54 +0000

    preloadedImages added

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 import "../styles/App.css";
 import Search from "./Search";
 import SearchResults from "./SearchResults";
+import preloadedImages from "../data/preloadedImages";
 
 function App() {
-  const [searchResults, setSearchResults] = useState();
+  const [searchResults, setSearchResults] = useState(preloadedImages);
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       <img
-        className="nasa-logo"
+        className="logo"
         src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
-        alt="nasaLogo"
+        alt="NASA logo"
+        data-testid="logo"
       />
       <Search setSearchResults={setSearchResults} />
-      <SearchResults />
+      <div className="search-results">
+        <SearchResults results={searchResults} />
+      </div>
     </div>
   );
 }
